3cf5491e8462ea7d793b4d8b90030454ddf1f18f
SvetaK7 <atevs7@inbox.ru>
2023-02-23 22:26:07 +0300

    clear form

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,25 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import s from "./Contacts.module.scss";
 import sContainer from './../common/styles/Container.module.css'
 import {MainTitle} from "../common/components/MainTitle";
 import {send} from "emailjs-com";
+import Stack from '@mui/material/Stack';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 function Contacts() {
 
-    // useEffect(() => {
-    //     document.addEventListener('submit', (e) => {
-    //         // e.preventDefault();
-    //         // @ts-ignore
-    //         console.log(e.target);
-    //         // @ts-ignore
-    //         e.target.reset();
-    //     })
-    // }, [])
     const [toSend, setToSend] = useState({
         from_name: '',
         message: '',
         reply_to: '',
     });
+    const [loading, setIsLoading] = useState(false)
 
     const onSubmit = (e: any) => {
         e.preventDefault();
@@ -31,16 +25,23 @@ function Contacts() {
         )
             .then((response: any) => {
                 console.log('SUCCESS!', response.status, response.text);
+                setToSend({
+                    from_name: '',
+                    message: '',
+                    reply_to: '',
+                })
             })
             .catch((err: any) => {
                 console.log('FAILED...', err);
             });
     };
 
+
     const handleChange = (e: any) => {
-        setToSend({ ...toSend, [e.target.name]: e.target.value });
+        setToSend({...toSend, [e.target.name]: e.target.value});
     };
 
+    // @ts-ignore
     return (
         <div id={'contacts'} className={s.contactsBlock}>
             <div className={`${sContainer.container}`}>
@@ -60,12 +61,33 @@ function Contacts() {
                                onChange={handleChange}
                         />
                         <input placeholder={'Message'}
-                                  type='text'
-                                  name='message'
-                                  value={toSend.message}
-                                  onChange={handleChange}
+                               type='text'
+                               name='message'
+                               value={toSend.message}
+                               onChange={handleChange}
                         ></input>
                         <button type={'submit'} className={s.buttonSend}>Send message</button>
+                        <Stack direction="row" spacing={2}>
+                            <LoadingButton loading={loading}
+
+                                           variant="outlined"
+                                           //disableRipple
+                                           sx={{width: 150,
+                                               backgroundColor: '#00acc1',
+                                               color: 'white',
+                                               borderColor: '#00acc1',
+                                               borderRadius: 1,
+                                               padding: 1,
+                                               '.Mui-focusVisible': {
+                                                   backgroundColor: '#00acc1!important'
+                                               },
+                                                  '.MuiButtonBase-root:hover': {
+                                                      backgroundColor: '#00acc1'
+                                           }
+                                           }}>
+                                Send message
+                            </LoadingButton>
+                        </Stack>
                     </form>
                 </div>
             </div>
